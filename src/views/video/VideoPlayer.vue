2bba3247f81e75447fbd4433befbbd205a5d8b9c
Kouseki <redstone_kun@foxmail.com>
2024-01-05 14:53:07 +0800

    Update VideoPlayer.vue

--- src/views/video/VideoPlayer.vue
+++ src/views/video/VideoPlayer.vue
@@ -209,6 +209,16 @@
                 </template>
                 <ul class="play-list">
                     <li class="play-item">
+                        <a :href="'ddplay://weblink/?url=' + encodeURI(url)" target="_blank">
+                            <n-tooltip trigger="hover">
+                                <template #trigger>
+                                    <img class="play-icon" src="/images/ddplay.webp" alt="">
+                                </template>
+                                IINA
+                            </n-tooltip>
+                        </a>
+                    </li>
+                    <li class="play-item">
                         <a :href="'iina://weblink/?url=' + urlBase" target="_blank">
                             <n-tooltip trigger="hover">
                                 <template #trigger>
